f0b9ddf4d6287b6b5b087a8276876e28d201bfc0
Nastya <alexsts2002@mail.ru>
2016-12-16 12:18:22 +0300

    Упс, другое имела в виду

--- build-mac.sh
+++ build-mac.sh
@@ -3,6 +3,6 @@ version=$1
 
 qmake
 make -j4
-macdeployqt FishServ/FishServ.app
-macdeployqt FishCli/FishCli.app
+#macdeployqt FishServ/FishServ.app
+#macdeployqt FishCli/FishCli.app
 tar -zcvf fishen-git-mac-${version}.tar.gz FishServ/FishServ.app FishCli/FishCli.app
