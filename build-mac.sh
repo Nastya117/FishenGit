7ad53b6ad5ad234cd5cffd21d6c893033eb1faa7
Nastya <alexsts2002@mail.ru>
2016-12-16 12:06:39 +0300

    Вроде равно творит что-то не то

--- build-mac.sh
+++ build-mac.sh
@@ -4,5 +4,5 @@ version=$1
 qmake
 make -j4
 macdeployqt FishServ/FishServ.app
-iacdeployqt FishCli/FishCli.app
+macdeployqt FishCli/FishCli.app
 tar -zcvf fishen-git-mac-${version}.tar.gz FishServ/FishServ.app FishCli/FishCli.app
