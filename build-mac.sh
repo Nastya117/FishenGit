1c30039c7f55b5a99cef188c159e626defaea790
Nastya <alexsts2002@mail.ru>
2016-12-16 11:54:45 +0300

    Вроде так работает

--- build-mac.sh
+++ build-mac.sh
@@ -5,4 +5,4 @@ qmake
 make -j4
 macdeployqt FishServ/FishServ.app
 iacdeployqt FishCli/FishCli.app
-zip FishenGit-mac-${version}.zip -r FishServ/FishServ.app FishCli/FishCli.app
+tar -zcvf fishen-git-mac-${version}.tar.gz FishServ/FishServ.app FishCli/FishCli.app
